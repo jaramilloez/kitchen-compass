125834ee44ec2ad43f22fba96bd9ccf198ea7383
Ezekiel Jaramillo <jaramilloez2004@gmail.com>
2024-08-26 14:25:05 -0600

    Finish recipe routes

--- back-end/routes/recipes.js
+++ back-end/routes/recipes.js
@@ -46,8 +46,38 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-router.delete("/", async (req, res) => {});
+  let ingredients = [];
+  const ingredientIds = req.body.ingredients;
+  for (let i = 0; i < ingredientIds.length; i++) {
+    const ingredientId = ingredientIds[i];
+    const ingredient = await User.findById(ingredientId);
+    if (!ingredient)
+      return res.status(400).send(`User with ID ${ingredientId} not found.`);
+
+    ingredients += ingredient;
+  }
+
+  const recipe = await Recipe.findByIdAndUpdate(req.params.id, {
+    $set: {
+      name: req.body.name,
+      description: req.body.description,
+      pic: req.body.pic,
+      directions: req.body.directions,
+      ingredients: ingredients,
+    },
+  });
+  if (!recipe) return res.status(404).send("Recipe not found.");
+  res.send(recipe);
+});
+
+router.delete("/:id", async (req, res) => {
+  const recipe = await Recipe.findByIdAndDelete(req.params.id);
+  if (!recipe) return res.status(404).send("Recipe not found.");
+  res.send(recipe);
+});
 
 module.exports = router;
