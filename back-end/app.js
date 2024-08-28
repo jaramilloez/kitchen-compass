d1326974a9f1cde5dafc68d8c8411993905a1163
Ezekiel Jaramillo <jaramilloez2004@gmail.com>
2024-08-28 12:04:09 -0600

    Update node-fetch add use cors

--- back-end/app.js
+++ back-end/app.js
@@ -1,18 +1,20 @@
-const express = require("express");
 const mongoose = require("mongoose");
-const fetch = require("node-fetch");
+const express = require("express");
 
-const categories = require("./routes/category");
-const groups = require("./routes/groups");
-const ingredients = require("./routes/ingredients");
-const recipes = require("./routes/recipes");
 const users = require("./routes/users");
+const recipes = require("./routes/recipes");
+const ingredients = require("./routes/ingredients");
+const groups = require("./routes/groups");
+const categories = require("./routes/category");
 
+const fetch = require("node-fetch");
 global.fetch = fetch;
 global.Headers = fetch.Headers || Headers;
 
 const app = express();
+require("./routes/cors")(app);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("./api/categories", categories);
 app.use("./api/groups", groups);
 app.use("./api/ingredients", ingredients);
