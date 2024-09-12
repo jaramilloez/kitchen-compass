8369458ce6582b120e810cb51d96dfd6d8c6ce25
Ezekiel Jaramillo <jaramilloez2004@gmail.com>
2024-09-11 19:59:42 -0600

    Display errors on log in page

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -38,9 +38,10 @@ router.post("/auth", async (req, res) => {
   const { email, password } = req.body;
 
   let user = await User.findOne({ email: email });
+  if (!user) return res.status(400).send("Invalid email or password.");
+
   const validPassword = await bcrypt.compare(password, user.password);
-  if (!user || !validPassword)
-    return res.status(400).send("Invalid email or password.");
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
 
   const token = user.generateAuthToken();
   res.send(token);
