3e7fb9f9e019e78f04e2680718585dae8a1c5d9c
Ezekiel Jaramillo <jaramilloez2004@gmail.com>
2024-09-15 22:44:37 -0600

    Make recipeCards a mapped array

--- front-end/src/components/recipeCards.jsx
+++ front-end/src/components/recipeCards.jsx
@@ -5,27 +5,28 @@ const RecipeCards = ({ data }) => {
   return (
     <div className="container p-4 m-0 col rounded-end-1 bg-white">
       <div className="row flex-wrap">
-        <Link
-          to="/recipe"
-          className="col-md-4 col-6 d-flex justify-content-center text-decoration-none p-2"
-        >
-          <div className="recipeCard card border-0">
-            <div className="position-relative">
-              <img
-                className="card-img-top"
-                src={require("../images/spaghetti.jpg")}
-                alt="Spaghetti"
-              />
-              <div className="cardDrawer bgLightGray position-absolute fs-5 opacity-75 w-100 p-3 overflow-hidden z-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        {data.map((item) => (
+          <Link
+            to={`/recipes/${item._id}`}
+            className="col-md-4 col-6 d-flex justify-content-center text-decoration-none p-2"
+          >
+            <div className="recipeCard card border-0">
+              <div className="position-relative">
+                <img
+                  className="card-img-top"
+                  src={item.pic}
+                  alt={item.description}
+                />
+                <div className="cardDrawer bgLightGray position-absolute fs-5 opacity-75 w-100 p-3 overflow-hidden z-0">
+                  {item.description}
+                </div>
+              </div>
+              <div className="card-body bgLightGray rounded-bottom-2 py-2 z-1">
+                <div className="card-title fs-4">{item.name}</div>
               </div>
             </div>
-            <div className="card-body bgLightGray rounded-bottom-2 py-2 z-1">
-              <div className="card-title fs-4">Spaghetti</div>
-            </div>
-          </div>
-        </Link>
+          </Link>
+        ))}
       </div>
     </div>
   );
